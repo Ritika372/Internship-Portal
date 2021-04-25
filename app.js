86d236f7993045b89c7e5583ad5823c38b59d98b
Ritika Goyal <54425330+Ritika372@users.noreply.github.com>
2021-04-25 16:40:39 +0530

    extra

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ app.use('/student', require('./routes/api/studentApi/student_routes'));
 app.use('/admin', require('./routes/api/Admin/admin_routes'));
 app.use('/company', require('./routes/api/Company/company_routes'));
 
-app.listen(3000, () => {
+const port = 3000 || process.env.PORT;
+app.listen(port, () => {
   console.log('Server started on port 3000');
 });
