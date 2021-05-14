8f6fd436b9388e09cfa2667a70e1d0615478414c
Ritika <ritikagoyal372@gmail.com>
2021-05-14 12:34:08 +0530

    emailsetup

--- routes/api/Company/email.js
+++ routes/api/Company/email.js
@@ -1,41 +1,39 @@
-const nodemailer = require('nodemailer');
-const express = require('express');
-const path = require('path');
-const ejs =require("ejs");
-require('dotenv').config();
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+const path = require("path");
+require("dotenv").config();
 
-const sendMails = (email, text,msg) =>{
+const sendMails = (email, text, msg) => {
+  var transporter = nodemailer.createTransport({
+    service: "SendGrid",
+    auth: {
+      user: process.env.SENDMAIL,
+      pass: process.env.SENDPASS,
+    },
+  });
 
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: process.env.SENDMAIL,
-          pass: process.env.SENDPASS
-        }
-      });
-
-     ejs.renderFile(path.join(__dirname + "../../../../views/confirmationMail.ejs"),{confirmation_link:text,msg:msg} ,function (err, data) {
-        if (err) {
+  ejs.renderFile(
+    path.join(__dirname + "../../../../views/confirmationMail.ejs"),
+    { confirmation_link: text, msg: msg },
+    function (err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        var mainOptions = {
+          from: "Ritika Goyal <ritikag.cs.18@nitj.ac.in>",
+          to: email,
+          subject: "Verifying your registration",
+          html: data,
+        };
+        transporter.sendMail(mainOptions, function (err, info) {
+          if (err) {
             console.log(err);
-        } else {
-            var mainOptions = {
-                from: process.env.SENDMAIL,
-                to: email,
-                subject: 'Verifying Your registration',
-                html: data
-            };
-            transporter.sendMail(mainOptions, function (err, info) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log('Message sent: ' + info.response);
-                }
-            });
-        }
-        
+          } else {
+            console.log("Message sent: " + info.response);
+          }
         });
-     
-     
-
+      }
+    }
+  );
 };
-module.exports = sendMails ;
+module.exports = sendMails;
